03eabd7005cd62a16d9713c3e9d8891ccf44558b
AmmonC <ammoncrapo@outlook.com>
2026-05-22 02:24:08 -0600

    move multiplication handling from cleaner to lexer for improved parsing

--- src/cleaner.cpp
+++ src/cleaner.cpp
@@ -25,12 +25,14 @@ std::string cleaner(std::string inputFunction){
 
     replaceAll(inputFunction, " ", "");
 
-    for (int n = 0; n < inputFunction.length() - 1; ++ n) { //creates int variable n and sets it equal to 0, and as long as n is less than the length of the inputFunction string -1 run the stuff run the loop and add 1 to n at the end
+/*    for (int n = 0; n < inputFunction.length() - 1; ++ n) { //creates int variable n and sets it equal to 0, and as long as n is less than the length of the inputFunction string -1 run the stuff run the loop and add 1 to n at the end
         if (std::isdigit(inputFunction[n]) && (inputFunction[n+1]) == 'x') //if the character in position n is a digit and the character in position n+1 is x run next, if not it does ++n
             inputFunction.insert(n+1, "*"); //if above is true it inserts * into the n+1 position, not replaces
         if (std::isalpha(inputFunction[n]) && (inputFunction[n+1]) == '(') //if the character in position n is alphanumeric and the character in position n+1 is ( run next, if not it does ++n
             inputFunction.insert(n+1, "*"); //if above is true it inserts * into the n+1 position, not replaces
-    }
+        if ((inputFunction[n]) == ')' && std::isalpha(inputFunction[n+1]) && ) //if the character in position n is a closed parenthesis and the character in position n+1 is alphanumeric run next, if not it does ++n
+            inputFunction.insert(n+1, "*"); //if above is true it inserts * into the n+1 position, not replaces
+    }*/ //commented out because I moved this functionality to the lexer
 
     outputFunction = inputFunction;
 
