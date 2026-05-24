09d17368e1c40ccb52341d04fa7b21f37a5d11aa
AmmonC <ammoncrapo@outlook.com>
2026-05-23 21:10:38 -0600

    Trying to make a multi pass token reduction parser from scratch

--- src/parserast.cpp
+++ src/parserast.cpp
@@ -4,46 +4,94 @@
 #include <cctype> //needed for isalpha
 #include <string> //imports the string library
 #include <vector> //imports the vector
+#include <cctype> //used for isalnum
 #include "config.h" //includes the config file containing debug variables
 #include "utils.h" //this has the containsSubstring function
 
 //start of a basic attempt to organize the tokens based on PEMDAS
+
+/*while the number of operators 
+    for the numper of positions
+        if i = operator with highest to lowest priority
+            if i was ( then we need to go through everything after I until we hit ) and add 1 to the main for loop for every operator contained in the ()
+                organize the operators in the parenthesis and their values, 
+                when we hit ) exit this loop and return to the for the number of operators loop
+            if pos i is ) i is greater than 0 and i-1 is alphanumeric
+                put pos i-1 in the calcvector  
+            put pos i in the calcvector
+            advance to the next priority operator
+
+*/
+
 std::vector < std::string > pemdas(std::vector<std::string> inputVector, int operatorCount) {
     std::vector < std::string > calcVector; //creates the vector where the correctly ordered functions and their calculations will be kept.
-    for (int n = 0; n < operatorCount; ++n) { //for the number of operators we have
-        for (int i = 0; i < inputVector.size(); ++i) {//look at every position in the input vector
-            if (inputVector[i] == "(") { //if i is (
-                calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator
-                calcVector.push_back(inputVector[i]); //add it to the top of its branch
-                while (inputVector[i]!= ")") {
-                    if (inputVector[i] == "^") { //if i is a ^
-                        calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
-                        calcVector.push_back(inputVector[i]); //add it to the back of the seat
+    std::string currentOperator; //a string to keep track of where we are in the function
+    std::string currentPriority; //A string to determine the current highest priority operator
+    int openParenthesis = 0
+    std::vector < std::string > operators = {"(", ")", "^", "*", "/", "+", "-", "="}; //makes a vector to hold operators
+    while (0 < operatorCount) { //while 0 is less than the operatorCount
+        for (int i = 0; i < operators.size(); i++) { //look at every position in the operators vector
+            currentPriority = operators[i]; //the current priority changes as we go down through the list to be sure we look at every possible operator in the right order
+            for (int n = 0; n < inputVector.size(); n++ ) { //look at every position in the input vector
+                currentOperator = inputVector[n]; //the current operator is in the nth position of the input function
+                if (currentOperator == currentPriority && currentOperator == "(" ){ //and if the the current operator matches our priority and is an open parenthesis
+                    if (0 < openParenthesis) { //if we have already had open parenthesis this must be an inset set of parenthesis
+                        calcVector.insert(calcVector.begin(), inputVector[n]); //inset the inset parenthesis into the beginning of the function
                     }
-                    else if (inputVector[i] == "*" || inputVector[i] == "/") { //if i is a * or /
-                        calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
-                        calcVector.push_back(inputVector[i]); //add it to the back of the ast
+                    openParenthesis += 1; //add one to the number of open parenthesis
+                    if ( 0 < n){ //
+                        calcVector.push_back(inputVector[n - 1]); 
                     }
-                    else if (inputVector[i] == "+" || inputVector[i] == "-") { //if i is a + or -
-                        calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
-                        calcVector.push_back(inputVector[i]); //add it to the back of the ast
-                    }
+                    calcVector.push_back(inputVector[n]);
                 }
-            }
-            if (inputVector[i] == "^") { //if i is a ^
-                calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
-                calcVector.push_back(inputVector[i]); //add it to the back of the seat
-            }
-            else if (inputVector[i] == "*" || inputVector[i] == "/") { //if i is a * or /
-                calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
-                calcVector.push_back(inputVector[i]); //add it to the back of the ast
-            }
-            else if (inputVector[i] == "+" || inputVector[i] == "-") { //if i is a + or -
-                calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
-                calcVector.push_back(inputVector[i]); //add it to the back of the ast
+                else if ()
             }
         }
     }
+    
+    /*for (int i = 0; i < inputVector.size(); ++i) {//look at every position in the input vector
+        if (inputVector[i] == "(") { //if i is (
+            calcVector.push_back(inputVector[i]); //add it to the top of its branch
+            if (config::debugMode) { //if debug is true
+                std::cout << "pemdas started ( loop." << std::endl;
+            }
+            while (inputVector[i]!= ")") {
+                if (inputVector[i] == "^") { //if i is a ^
+                    calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
+                    calcVector.push_back(inputVector[i]); //add it to the back of the seat
+                    i++; //advance to the next position of i
+                }
+                else if (inputVector[i] == "*" || inputVector[i] == "/") { //if i is a * or /
+                    calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
+                    calcVector.push_back(inputVector[i]); //add it to the back of the ast
+                    i++; //advance to the next position of i
+                }
+                else if (inputVector[i] == "+" || inputVector[i] == "-") { //if i is a + or -
+                    calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
+                    calcVector.push_back(inputVector[i]); //add it to the back of the ast
+                    i++; //advance to the next position of i
+                }
+                else;
+                    calcVector.push_back(inputVector[i]); //add it to the back of the ast
+                    i++; //advance to the next position of i
+            }
+            if (config::debugMode) { //if debug is true
+                std::cout << "pemdas finished ) loop." << std::endl;
+            }
+        }
+        if (inputVector[i] == "^") { //if i is a ^
+            calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
+            calcVector.push_back(inputVector[i]); //add it to the back of the seat
+        }
+        else if (inputVector[i] == "*" || inputVector[i] == "/") { //if i is a * or /
+            calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
+            calcVector.push_back(inputVector[i]); //add it to the back of the ast
+        }
+        else if (inputVector[i] == "+" || inputVector[i] == "-") { //if i is a + or -
+            calcVector.push_back(inputVector[i - 1]); //grabs the number before the operator to the back of the ast
+            calcVector.push_back(inputVector[i]); //add it to the back of the ast
+        }
+    }*/
     return calcVector;
 }
 
